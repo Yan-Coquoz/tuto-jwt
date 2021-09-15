479f653138a40f6971a0ac4c30065eec9976a345
Yan Coquoz <Yan.Coquoz@gmail.com>
2021-09-15 13:24:01 +0200

    Base De JWT

---
+++ index.js
@@ -0,0 +1,31 @@
+require("dotenv").config();
+const JWT = require("jsonwebtoken");
+
+// creation d'un user pour l'authentification,
+/**
+ * On créé le compte et l'inserer dans une Bdd
+ * Ensuite on va le chercher et on lui créé le token
+ */
+
+const user = {
+  id: 5,
+  firstname: "Roland",
+  lastname: "Garros",
+  age: 32,
+  email: "rg@tennis.fr",
+  admin: true,
+};
+
+/**
+ * Création de la fonction pour l'acces des droits
+ * prend un user en parametre
+ */
+
+function tokenAccess(user) {
+  // donne un token à l'utilisateur, la clé secrete, et la duré d'expiration en secondes (1800 s = 30 min)
+  return JWT.sign(user, process.env.JWT_SECRET, { expiresIn: "1800" });
+}
+// on stock la valeur du token
+const accessToken = tokenAccess(user);
+// Ce token contient toutes les infos de l'objet user
+console.log("Le token crypté : ", accessToken);
