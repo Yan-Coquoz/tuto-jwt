bc9a1dd2657d5d3c66c85a78a09722a353cd48fd
Yan Coquoz <Yan.Coquoz@gmail.com>
2021-09-15 14:26:49 +0200

    Mise en place de l'ecoute et la premiere route app.post

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const express = require("express");
 const { urlencoded } = require("express");
 const app = express();
 
+const PORT = process.env.PORT || 3000;
+
 /**
  * Express va recupérer du Json
  * urlencoded pour la bonne gestion des accents
@@ -35,7 +37,32 @@ function tokenAccess(user) {
   // donne un token à l'utilisateur, la clé secrete, et la duré d'expiration en secondes (1800 s = 30 min)
   return JWT.sign(user, process.env.JWT_SECRET, { expiresIn: "1800" });
 }
-// on stock la valeur du token
-const accessToken = tokenAccess(user);
-// Ce token contient toutes les infos de l'objet user
-console.log("Le token crypté : ", accessToken);
+// // on stock la valeur du token
+// const accessToken = tokenAccess(user);
+// // Ce token contient toutes les infos de l'objet user
+// console.log("Le token crypté : ", accessToken);
+
+// Les routes
+
+app.post("/api/login", (req, res) => {
+  // On check la recuperation des données
+  if (req.body.email !== user.email) {
+    res.status(401).send("invalid credentials");
+    // on return pour stoppé le proccess
+    return;
+  }
+  if (req.body.password !== "MDPmotdepasse") {
+    res.status(401).send("invalid credentials");
+    // on return pour stoppé le proccess
+    return;
+  }
+  // si c'est bon on génère l'accessToken
+  const accessToken = tokenAccess(user);
+  res.send({
+    accessToken,
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Listenning on port ${PORT}`);
+});
